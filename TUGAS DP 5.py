770ec3d83fe9c8b7d460762ff6da25e7c7c9cb68
Alfaruq <114473569+alfaruqPS@users.noreply.github.com>
2022-10-18 19:56:49 +0700

    Add files via upload
    tugas sesi 5 dasar pemrograman input nama

---
+++ TUGAS DP 5.py
@@ -0,0 +1,10 @@
+# tugas input biodata
+
+print("Input Biodata")
+
+nama = input("Masukan Nama  : ")
+kelas = input("Masukan Kelas  : ")
+nim =   input("Masukan Nim  : ")
+jurusan = input("Masukan Jurusan  :")
+
+print("Halo, Nama Saya %s, Kelas Saya  %s, Nim %s,  Jurusan %s" % (nama, kelas, nim, jurusan))
